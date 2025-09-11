9b5265a409fe3a4cd8aa907fc7febd59894e19bf
李荣春 <lirongchun@lirongcundeMini.lan>
2025-09-11 15:36:32 +0800

    fix: update decap cms config and add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://lx521603.github.io', // 您的 GitHub Pages URL
+  base: '/popular-photon/',           // 您的仓库名称
+});
